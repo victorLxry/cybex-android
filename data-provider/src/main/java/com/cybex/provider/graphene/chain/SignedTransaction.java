60fd22ad09085737fc5787e453d0ff89c0c7e517
shefengl <shefengl@mtu.edu>
2019-01-21 17:26:09 +0800

    :sparkles: 添加QR code 压缩，get_recent_transaction_by_id

--- data-provider/src/main/java/com/cybex/provider/graphene/chain/SignedTransaction.java
+++ data-provider/src/main/java/com/cybex/provider/graphene/chain/SignedTransaction.java
@@ -3,13 +3,15 @@ package com.cybex.provider.graphene.chain;
 import android.util.Log;
 
 import com.cybex.provider.crypto.Sha256Object;
+import com.cybex.provider.utils.MyUtils;
+import com.google.common.io.BaseEncoding;
 
 import java.util.ArrayList;
 import java.util.List;
 
 public class SignedTransaction extends Transaction {
     private final static char[] hexArray = "0123456789abcdef".toCharArray();
-    transient List<CompactSignature> SignaturesBuffer = new ArrayList<>();
+    public transient List<CompactSignature> SignaturesBuffer = new ArrayList<>();
     List<String> signatures = new ArrayList<>();
 
     public void sign(Types.private_key_type privateKeyType, Sha256Object chain_id) {
@@ -24,6 +26,16 @@ public class SignedTransaction extends Transaction {
         signatures.add(bytesToHex(privateKeyType.getPrivateKey().sign_compact(digest, true).data));
         Log.e("withdraw_deposit_hash", signatures.get(0));
         return signatures.get(0);
+    }
+
+    public String getTransactionID() {
+        if (SignaturesBuffer != null) {
+            Sha256Object signDigest = sig_digest_with_signature();
+            byte[] transactionId = new byte[20];
+            System.arraycopy(signDigest.hash, 0, transactionId, 0, transactionId.length);
+            return MyUtils.bytesToHex(transactionId);
+        }
+        return null;
     }
 
     private String bytesToHex(byte[] bytes) {
